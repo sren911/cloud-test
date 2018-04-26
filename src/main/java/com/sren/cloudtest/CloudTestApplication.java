67535359d64d53763add85f599dafa50e6e30506
renshuai <renshuai@jiuqi.com.cn>
2018-04-26 13:27:48 +0800

    Initial commit

---
+++ src/main/java/com/sren/cloudtest/CloudTestApplication.java
@@ -0,0 +1,12 @@
+package com.sren.cloudtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudTestApplication.class, args);
+	}
+}
